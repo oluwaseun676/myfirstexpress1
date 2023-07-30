0fbe1276104f8e4be2246685ca2a43fa80657396
oreoluwa <oluwaseunolaniran01@gmail.com>
2023-07-30 19:24:36 +0100

    Server changed

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const body_parser = require("body-parser");
 const frontend = require("./html_route");
 const userRoute = require("./endpoint/miamiRoute");
 const PORT = 3180;
+const Home = require("./frontend/html/miami.html");
+
 const app = express();
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
@@ -15,9 +17,10 @@ app.get("/", (req, res) => {
   res.set("Content-Type", "text/html");
   res.status(200);
   res.statusMessage = "Everything is okay with the request";
-    const body = `<h2>Welcome To Ore's Relaxation Hub</h2>
-       <p>Your one stop relaxation hub</p>`;
-  res.send(body);
+    //const body = `<h2>Welcome To Ore's Relaxation Hub</h2>
+    //   <p>Your one stop relaxation hub</p>`;
+  //res.send(body);
+    res.write(Home)
 });
 
 // api route
