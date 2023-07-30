6e48b472cbdcb22b44bf49069a46d9e838614bc3
oreoluwa <oluwaseunolaniran01@gmail.com>
2023-07-30 19:12:14 +0100

    Miami.js added

---
+++ frontend/miami.js
@@ -0,0 +1,16 @@
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const services = document.querySelector("#services");
+services.addEventListener("click", (evt) =>{
+  evt.preventDefault()
+  window.location.href = "/frontend/menu"
+})
+about.addEventListener("click",(evt)=>{
+  evt.preventDefault();
+  window.location.href = "/frontend/about";
+})
+home.addEventListener("click",(evt)=>{
+  evt.preventDefault();
+  window.location.href = "/frontend/";
+}
+)
