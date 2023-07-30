0913233a0536a0d604603069dddf70320cf977cf
oreoluwa <oluwaseunolaniran01@gmail.com>
2023-07-30 19:21:08 +0100

    Miami updated

--- frontend/miami.js
+++ frontend/miami.js
@@ -1,6 +1,6 @@
-const home = document.querySelector("#home");
-const about = document.querySelector("#about");
-const services = document.querySelector("#services");
+const home = document.querySelector(".home");
+const about = document.querySelector(".about");
+const services = document.querySelector(".services");
 services.addEventListener("click", (evt) =>{
   evt.preventDefault()
   window.location.href = "/frontend/menu"
